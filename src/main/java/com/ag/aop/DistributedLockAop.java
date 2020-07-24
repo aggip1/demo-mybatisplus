55dfe1b963f8b471625fa89641955213a5fb528b
zsw <1355796120@qq.com>
2020-07-24 12:37:21 +0800

    添加分布式锁，redis配置没配

--- src/main/java/com/ag/aop/DistributedLockAop.java
+++ src/main/java/com/ag/aop/DistributedLockAop.java
@@ -1,23 +1,47 @@
 package com.ag.aop;
 
+import com.ag.annotation.DistributedLock;
+import com.ag.utils.LockUtils;
+import com.github.pagehelper.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
-//import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-//@Aspect
+import java.time.Duration;
+
+@Aspect
 @Component
 @Slf4j
 public class DistributedLockAop {
 
-//    @Autowired
-//    private RedisTemplate<String,Object> redisTemplate;
-//    @Around("@annotation(lock)")
-//    public Object doAround(ProceedingJoinPoint proceedingJoinPoint, DistributedLock lock){
-//        String key = lock.key();
-//        if(StringUtil.isNotEmpty(key)){
-//
-//        }
-//        return null;
-//    }
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Around("@annotation(lock)")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint, DistributedLock lock) {
+        String key = lock.key();
+        if (StringUtil.isNotEmpty(key)) {
+            key = proceedingJoinPoint.getTarget().getClass().toString() + ":" + proceedingJoinPoint.getSignature().getName();
+        }
+        Duration timeout = Duration.ofMillis(lock.timeout());
+        Object proceed = null;
+        if (LockUtils.tryLock(redisTemplate, key, timeout)) {
+            try {
+                proceed = proceedingJoinPoint.proceed();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+                log.debug("分布式锁加锁以后逻辑处理失败");
+            }finally {
+                //释放锁
+                LockUtils.release(redisTemplate,key);
+            }
+        }
+        return proceed;
+
+    }
 
 }
---
+++ src/main/java/com/ag/utils/LockUtils.java
@@ -0,0 +1,39 @@
+package com.ag.utils;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+import java.util.Objects;
+
+//todo
+@UtilityClass
+public class LockUtils {
+    public boolean tryLock(RedisTemplate<String, Object> redisTemplate, String key, Duration duration) {
+
+        return (Boolean) redisTemplate.execute((RedisCallback) connection->{
+
+            long expire = System.currentTimeMillis() + duration.toMillis();
+            Boolean acquire = connection.setNX(key.getBytes(), String.valueOf(expire).getBytes());
+            if(acquire){
+                return true;
+            }
+            byte[] bytes = connection.get(key.getBytes());
+            if(Objects.nonNull(bytes)&&bytes.length>0){
+                long expireTime = Long.parseLong(new String(bytes));
+                if(expireTime<System.currentTimeMillis()){
+                    connection.getSet(key.getBytes(),String.valueOf(expire).getBytes() );
+                    return true;
+                }
+            }
+            return false;
+        });
+
+
+    }
+
+    public static void release(RedisTemplate<String, Object> redisTemplate, String key) {
+         redisTemplate.delete(key);
+    }
+}
